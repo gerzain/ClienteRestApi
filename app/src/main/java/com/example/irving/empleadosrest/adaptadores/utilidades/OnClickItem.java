f55fd8c7e105fa591e1e608e6f749ba07fc7f896
Irving <irving.java@gmail.com>
2017-03-17 18:49:29 -0600

    Agregar Interface para evento OnClick

---
+++ app/src/main/java/com/example/irving/empleadosrest/adaptadores/utilidades/OnClickItem.java
@@ -0,0 +1,12 @@
+package com.example.irving.empleadosrest.adaptadores.utilidades;
+
+import com.example.irving.empleadosrest.modelos.Empleado;
+
+/**
+ * Created by Irving on 17/03/2017.
+ */
+public interface OnClickItem
+{
+    void onItemClick(Empleado empleado);
+
+}
